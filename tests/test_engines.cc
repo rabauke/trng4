c9bc47abb9125e558686d01ccfd972121387b743
Heiko Bauke <heiko.bauke@mail.de>
2019-09-06 23:28:36 +0200

    add unit tests

--- tests/test_engines.cc
+++ tests/test_engines.cc
@@ -1,8 +1,10 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <tuple>
+
 #define BOOST_TEST_MODULE test engines
 #define BOOST_TEST_DYN_LINK
-
-#include <vector>
-#include <sstream>
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/list.hpp>
 
@@ -27,15 +29,24 @@
 #include <trng/mt19937.hpp>
 #include <trng/mt19937_64.hpp>
 
-typedef boost::mpl::list<trng::lcg64, trng::lcg64_shift,  //
-                         trng::mrg2, trng::mrg3, trng::mrg3s, trng::mrg4, trng::mrg5,
-                         trng::mrg5s,  //
-                         trng::yarn2, trng::yarn3, trng::yarn3s, trng::yarn4, trng::yarn5,
-                         trng::yarn5s,                                        //
-                         trng::lagfib2xor_521_64, trng::lagfib4xor_521_32,    //
-                         trng::lagfib2plus_521_32, trng::lagfib4plus_521_64,  //
-                         trng::mt19937, trng::mt19937_64>
-    engines;
+using engines =
+    boost::mpl::list<trng::lcg64, trng::lcg64_shift,  //
+                     trng::mrg2, trng::mrg3, trng::mrg3s, trng::mrg4, trng::mrg5,
+                     trng::mrg5s,  //
+                     trng::yarn2, trng::yarn3, trng::yarn3s, trng::yarn4, trng::yarn5,
+                     trng::yarn5s,                                        //
+                     trng::lagfib2xor_521_64, trng::lagfib4xor_521_32,    //
+                     trng::lagfib2plus_521_32, trng::lagfib4plus_521_64,  //
+                     trng::mt19937, trng::mt19937_64>;
+
+using parallel_engines =
+    boost::mpl::list<trng::lcg64, trng::lcg64_shift,  //
+                     trng::mrg2, trng::mrg3, trng::mrg3s, trng::mrg4, trng::mrg5,
+                     trng::mrg5s,  //
+                     trng::yarn2, trng::yarn3, trng::yarn3s, trng::yarn4, trng::yarn5,
+                     trng::yarn5s>;
+
+using floats = boost::mpl::list<float, double, long double>;
 
 
 template<typename R>
@@ -55,7 +66,7 @@ void advance_engine(R &r1, R &r2, const long N) {
 
 template<typename R>
 std::tuple<std::vector<typename R::result_type>, std::vector<typename R::result_type>>
-generate_list(R &r1, R &r2, const long N) {
+generate_list(R &r1, R &r2, const long N, const long skip1, const long skip2) {
   std::vector<typename R::result_type> v1, v2;
   if (N > 0) {
     v1.reserve(N);
@@ -64,12 +75,26 @@ generate_list(R &r1, R &r2, const long N) {
   for (long i{0}; i < N; ++i) {
     v1.push_back(r1());
     v2.push_back(r2());
+    for (long j{0}; j < skip1; ++j)
+      r1();
+    for (long j{0}; j < skip2; ++j)
+      r2();
   }
   return {v1, v2};
 }
 
 
+template<typename R>
+std::tuple<std::vector<typename R::result_type>, std::vector<typename R::result_type>>
+generate_list(R &r1, R &r2, const long N) {
+  return generate_list(r1, r2, N, 0, 0);
+}
+
+//-----------------------------------------------------------------------------------------
+
 BOOST_AUTO_TEST_SUITE(test_suite_engines)
+
+//-----------------------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE(test_suite_advance)
 BOOST_AUTO_TEST_CASE_TEMPLATE(test_advance, R, engines) {
@@ -79,6 +104,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_advance, R, engines) {
   BOOST_TEST(r1 != r2, "engines have different state");
 }
 BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE(test_suite_restore)
 BOOST_AUTO_TEST_CASE_TEMPLATE(test_restore, R, engines) {
@@ -96,13 +123,15 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_restore, R, engines) {
 }
 BOOST_AUTO_TEST_SUITE_END()
 
+//-----------------------------------------------------------------------------------------
+
 BOOST_AUTO_TEST_SUITE(test_suite_status_io)
 BOOST_AUTO_TEST_CASE_TEMPLATE(test_status_io, R, engines) {
   // two engines with equal state
   R r1, r2;
   advance_engine(r1, 271828l);  // advance engine r1
   std::stringstream str;
-  str << r1 << '\n';
+  str << r1;
   str >> r2;
   BOOST_TEST(r1 == r2, "engine state restored from stream successfully");
   // when state has been restored correctly, r1 and r2 must yield the same values
@@ -111,5 +140,125 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_status_io, R, engines) {
              "engines yield same values after restore from stream");
 }
 BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------------------
+
+template<typename R>
+struct generator_min_max {
+  typedef typename R::result_type result_type;
+  static constexpr result_type min() { return R::min(); }
+  static constexpr result_type max() { return R::max(); }
+};
+
+template<typename R>
+struct generator_min : public generator_min_max<R> {
+  using base = generator_min_max<R>;
+  typename base::result_type operator()() const { return base::min(); }
+};
+
+template<typename R>
+struct generator_max : public generator_min_max<R> {
+  using base = generator_min_max<R>;
+  typename base::result_type operator()() const { return base::max(); }
+};
+
+template<typename T>
+struct type_name;
+
+template<>
+struct type_name<float> {
+  static const std::string name;
+};
+
+const std::string type_name<float>::name = "float";
+
+template<>
+struct type_name<double> {
+  static const std::string name;
+};
+
+const std::string type_name<double>::name = "double";
+
+template<>
+struct type_name<long double> {
+  static const std::string name;
+};
+
+const std::string type_name<long double>::name = "long double";
+
+template<typename R, typename T>
+void test_ranges_impl() {
+  const std::string &name{type_name<T>::name};
+  generator_min<R> r_min;
+  generator_max<R> r_max;
+  using u01xx_min = trng::utility::u01xx_traits<T, 1, generator_min<R>>;
+  using u01xx_max = trng::utility::u01xx_traits<T, 1, generator_max<R>>;
+  T x_min{}, x_max{};
+  x_min = u01xx_min::cc(r_min);
+  x_max = u01xx_max::cc(r_max);
+  BOOST_TEST((x_min >= 0 && x_max <= 1), "[0, 1] bounds ok for " + name);
+  x_min = u01xx_min::co(r_min);
+  x_max = u01xx_max::co(r_max);
+  BOOST_TEST((x_min >= 0 and x_max < 1), "[0, 1) bounds ok for " + name);
+  x_min = u01xx_min::oc(r_min);
+  x_max = u01xx_max::oc(r_max);
+  BOOST_TEST((x_min > 0 and x_max <= 1), "(0, 1] bounds ok for " + name);
+  x_min = u01xx_min::oo(r_min);
+  x_max = u01xx_max::oo(r_max);
+  BOOST_TEST((x_min > 0 and x_max < 1), "(0, 1) bounds ok for " + name);
+}
+
+BOOST_AUTO_TEST_SUITE(test_suite_ranges)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_ranges, R, engines) {
+  test_ranges_impl<R, float>();
+  test_ranges_impl<R, double>();
+  test_ranges_impl<R, long double>();
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(test_suite_jump2)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_jump2, R, parallel_engines) {
+  // two engines with equal state
+  R r1, r2;
+  long n{1l};
+  // jump ahead in steps of 2^i or step forwards in unit steps
+  for (long i{0l}; i < 20l; ++i) {
+    r1.jump2(i);
+    for (long j{0l}; j < n; ++j)
+      r2();
+    std::stringstream message;
+    message << "engines have equal state after jump2(" << i << ")";
+    BOOST_TEST(r1 == r2, message.str().c_str());
+    n <<= 1;
+  }
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(test_suite_split)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_split, R, parallel_engines) {
+  for (long i{2l}; i <= 20l; ++i) {
+    for (long j{0l}; j < i; ++j) {
+      // two engines with equal state
+      R r1, r2;
+      // split or step forwards in j unit steps
+      for (long k{0l}; k < j; ++k)
+        r1();
+      r2.split(i, j);  // split into i streams, pick stream j
+      {
+        auto v = generate_list(r1, r2, 32, i - 1, 0);
+        std::stringstream message;
+        message << "engines yield same values after split(" << i << ", " << j << ")";
+        BOOST_TEST(std::get<0>(v) == std::get<1>(v), message.str().c_str());
+      }
+    }
+  }
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE_END()
