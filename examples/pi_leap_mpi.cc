5e41ca15acd0414ffccdf8f9ae73168bba059068
Heiko Bauke <heiko.bauke@mail.de>
2019-09-29 17:52:56 +0200

    use C API of the MPI standard (instead of the depreciated C++ API), fix issue #15

--- examples/pi_leap_mpi.cc
+++ examples/pi_leap_mpi.cc
@@ -37,29 +37,30 @@
 #include <trng/uniform01_dist.hpp>
 
 int main(int argc, char *argv[]) {
-  const long samples = 1000000l;          // total number of points in square
-  trng::yarn2 rx, ry;                     // random number engines for x- and y-coordinates
-  MPI::Init(argc, argv);                  // initialize MPI environment
-  int size = MPI::COMM_WORLD.Get_size();  // get total number of processes
-  int rank = MPI::COMM_WORLD.Get_rank();  // get rank of current process
+  const long samples{1000000l};  // total number of points in square
+  MPI_Init(&argc, &argv);        // initialize MPI environment
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);  // get total number of processes
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // get rank of current process
+  trng::yarn2 rx, ry;                    // random number engines for x- and y-coordinates
   // split PRN sequences by leapfrog method
   rx.split(2, 0);            // choose sub-stream no. 0 out of 2 streams
   ry.split(2, 1);            // choose sub-stream no. 1 out of 2 streams
   rx.split(size, rank);      // choose sub-stream no. rank out of size streams
   ry.split(size, rank);      // choose sub-stream no. rank out of size streams
-  long in = 0l;              // number of points in circle
+  long in{0l};               // number of points in circle
   trng::uniform01_dist<> u;  // random number distribution
   // throw random points into square and distribute workload over all processes
-  for (long i = rank; i < samples; i += size) {
-    double x = u(rx), y = u(ry);  // choose random x- and y-coordinates
-    if (x * x + y * y <= 1.0)     // is point in circle?
-      ++in;                       // increase counter
+  for (long i{rank}; i < samples; i += size) {
+    const double x{u(rx)}, y{u(ry)};  // choose random x- and y-coordinates
+    if (x * x + y * y <= 1.0)         // is point in circle?
+      ++in;                           // increase counter
   }
   // calculate sum of all local variables 'in' and storre result in 'in_all' on process 0
   long in_all;
-  MPI::COMM_WORLD.Reduce(&in, &in_all, 1, MPI::LONG, MPI::SUM, 0);
+  MPI_Reduce(&in, &in_all, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
   if (rank == 0)  // print result
     std::cout << "pi = " << 4.0 * in_all / samples << std::endl;
-  MPI::Finalize();  // quit MPI
+  MPI_Finalize();  // quit MPI
   return EXIT_SUCCESS;
 }
